0e41e2ae815f5d97196996b89c9f139dc6d9259f
adarshsharma-me <hello@adarshsharma.me>
2023-01-01 08:11:32 +0530

    Half Done

--- index.js
+++ index.js
@@ -1,28 +1,76 @@
 import { Configuration, OpenAIApi } from "openai";
-function search() {
-  var prompt = document.getElementById("comic-idea").value;
+let prompt = "";
+let dataResponse = "";
+const configuration = new Configuration({
+  apiKey: "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",
+});
 
-  for (i = 0; i < 10; i++) {
-    var img = document.createElement("img");
-    img.src = "./graduation.jpg";
-    document.getElementById("comic-gallery").appendChild(img);
-  }
-}
+document.querySelector("#searchButton").addEventListener("click", () => {
+  prompt = document.getElementById("comic-idea").value;
 
-// const configuration = new Configuration({
-//   apiKey: "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",
-// });
-// const openai = new OpenAIApi(configuration);
-// const prompt = "a lonely boy on a hill";
-// const result = await openai.createImage({
-//   prompt,
-//   n: 1,
-//   size: "1024x1024",
-// });
-// const url = result.data.data[0].url;
-// console.log(url);
+  fetch("http://localhost:8000/api", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ prompt }),
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      dataResponse = data.response.choices[0].text;
+      console.log(dataResponse);
+    });
+});
 
-// const imgResult = await fetch(url);
-// const blob = await imgResult.blob();
-// const buffer = Buffer.from( await blob.arrayBuffer())
-// writeFileSync( './img/ error because I${Date.now()}.png', buffer);
+document.querySelector("#drawButton").addEventListener("click", async () => {
+  const openai = new OpenAIApi(configuration);
+
+  const prompt = "a lonely boy on a hill";
+
+  const result = await openai.createImage({
+    prompt,
+    n: 1,
+    size: "1024x1024",
+  });
+  const url = result.data.data[0].url;
+
+  var img = document.createElement("img");
+  img.src = url;
+  document.getElementById("comic-gallery").appendChild(img);
+  // console.log(url);
+});
+
+// function draw2() {
+//   const openai = new OpenAIApi(configuration);
+//   const pr = [
+//     "a guy riding a horse",
+//     "a man plugging his laptop",
+//     "shit and fly relationship",
+//   ];
+//   let prd;
+
+//   for (let x in pr) {
+//     prd = pr[x];
+//     async function myFunction(prd) {
+//       const prompt = "a lonely boy on a hill";
+//       const result = await openai.createImage({
+//         prompt,
+//         n: 1,
+//         size: "1024x1024",
+//       });
+//       const url = result.data.data[0].url;
+//       console.log(url);
+
+//       const imgResult = await fetch(url);
+//       console.log(imgResult);
+//     }
+//   }
+// }
+
+// function search() {
+//   prompt = document.getElementById("comic-idea").value;
+//   // for (i = 0; i < 10; i++) {
+//   //   var img = document.createElement("img");
+//   //   img.src = "./graduation.jpg";
+//   //   document.getElementById("comic-gallery").appendChild(img);
+//   // }
